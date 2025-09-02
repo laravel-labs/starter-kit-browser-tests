8d78d0783e7f14ebefc117ebe0dee76842395df8
Mior Muhammad Zaki <crynobone@gmail.com>
2025-09-02 11:22:31 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Browser/RegisterUserTest.php
+++ tests/Browser/RegisterUserTest.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Models\User;
+
 test('it can show the register page', function () {
     visit('/register')
         ->assertNoConsoleLogs()
@@ -18,4 +20,33 @@ test('it can register a new user', function () {
         ->assertPathEndsWith('/dashboard');
 
     $this->assertAuthenticated();
+});
+
+test('it cannot register a new user when email has already been taken', function () {
+    User::factory()->create([
+        'name' => 'Taylor',
+        'email' => 'taylor@laravel.com',
+    ]);
+
+    visit('/register')
+        ->fill('name', 'Taylor Otwell')
+        ->fill('email', 'taylor@laravel.com')
+        ->fill('password', 'password')
+        ->fill('password_confirmation', 'password')
+        ->press('Create account')
+        ->assertSee('The email has already been taken.');
+
+    $this->assertGuest();
+});
+
+test('it cannot register a new user when password does not match', function () {
+    visit('/register')
+        ->fill('name', 'Taylor Otwell')
+        ->fill('email', 'taylor@laravel.com')
+        ->fill('password', 'password')
+        ->fill('password_confirmation', 'secret')
+        ->press('Create account')
+        ->assertSee('The password field confirmation does not match.');
+
+    $this->assertGuest();
 });
--- tests/Browser/WelcomeTest.php
+++ tests/Browser/WelcomeTest.php
@@ -9,15 +9,22 @@ test('it can show the default welcome page', function () {
         ->assertSee('Register');
 });
 
-
 test('it can browse to the register page', function () {
     visit('/')
-        ->assertNoConsoleLogs()
-        ->assertNoJavaScriptErrors()
         ->click('Register')
         ->assertPathEndsWith('/register')
         ->assertNoConsoleLogs()
         ->assertNoJavaScriptErrors()
         ->assertSee('Create an account')
         ->assertSee('Enter your details below to create your account');
+});
+
+test('it can browse to the login page', function () {
+    visit('/')
+        ->click('Log in')
+        ->assertPathEndsWith('/login')
+        ->assertNoConsoleLogs()
+        ->assertNoJavaScriptErrors()
+        ->assertSee('Log in to your account')
+        ->assertSee('Enter your email and password below to log in');
 });
