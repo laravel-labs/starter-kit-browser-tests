e02a357094c6914dd1f7f7a87d043f7671234ec9
Mior Muhammad Zaki <crynobone@gmail.com>
2025-09-02 10:13:43 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Unit/ExampleTest.php
+++ tests/Unit/ExampleTest.php
@@ -1,7 +1,5 @@
 <?php
 
 test('that true is true', function () {
-    expect(isset($_SERVER['APP_BASE_PATH']))->toBeTrue();
-    expect(isset($_ENV['APP_BASE_PATH']))->toBeTrue();
     expect(true)->toBeTrue();
 });
