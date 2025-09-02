8da7ee836a60b03858481f611cfecdf964d81076
Mior Muhammad Zaki <crynobone@gmail.com>
2025-09-02 11:03:21 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/Browser/RegisterUserTest.php
+++ tests/Browser/RegisterUserTest.php
@@ -2,6 +2,20 @@
 
 test('it can show the register page', function () {
     visit('/register')
+        ->assertNoConsoleLogs()
+        ->assertNoJavaScriptErrors()
         ->assertSee('Create an account')
         ->assertSee('Enter your details below to create your account');
+});
+
+test('it can register a new user', function () {
+    visit('/register')
+        ->fill('name', 'Taylor Otwell')
+        ->fill('email', 'taylor@laravel.com')
+        ->fill('password', 'password')
+        ->fill('password_confirmation', 'password')
+        ->press('Create account')
+        ->assertPathEndsWith('/dashboard');
+
+    $this->assertAuthenticated();
 });
--- tests/Browser/WelcomeTest.php
+++ tests/Browser/WelcomeTest.php
@@ -2,7 +2,22 @@
 
 test('it can show the default welcome page', function () {
     visit('/')
+        ->assertNoConsoleLogs()
+        ->assertNoJavaScriptErrors()
         ->assertSee('Let\'s get started')
         ->assertSee('Log In')
         ->assertSee('Register');
+});
+
+
+test('it can browse to the register page', function () {
+    visit('/')
+        ->assertNoConsoleLogs()
+        ->assertNoJavaScriptErrors()
+        ->click('Register')
+        ->assertPathEndsWith('/register')
+        ->assertNoConsoleLogs()
+        ->assertNoJavaScriptErrors()
+        ->assertSee('Create an account')
+        ->assertSee('Enter your details below to create your account');
 });
