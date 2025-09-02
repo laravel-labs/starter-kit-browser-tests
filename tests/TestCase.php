ff0287e53e980608a904b0720a57be9d530312e2
Mior Muhammad Zaki <crynobone@gmail.com>
2025-09-02 10:21:47 +0800

    wip
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,5 +8,14 @@ use PHPUnit\Framework\Attributes\RequiresEnvironmentVariable;
 #[RequiresEnvironmentVariable('APP_BASE_PATH')]
 abstract class TestCase extends BaseTestCase
 {
-    //
+    /** {@inheritdoc} */
+    #[\Override]
+    public function createApplication()
+    {
+        if (! isset($_ENV['APP_BASE_PATH']) && isset($_SERVER['APP_BASE_PATH'])) {
+            $_ENV['APP_BASE_PATH'] = $_SERVER['APP_BASE_PATH'];
+        }
+
+        return parent::createApplication();
+    }
 }
